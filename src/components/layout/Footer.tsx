e3a78e7bbbca5a6d88b4fe814d48972693908e9d
ImJaeOne <dlawi0108@naver.com>
2025-03-11 10:25:23 +0900

    🎉 Add: footer 구현 및 css

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="text-center fixed bottom-0 w-full bg-gray-800 text-white text-sm p-4 font-normal">
+      [lol-info-app] is not endorsed by Riot Games and does not reflect the
+      views or opinions of Riot Games or anyone officially involved in producing
+      or managing Riot Games properties. Riot Games and all associated
+      properties are trademarks or registered trademarks of Riot Games, Inc.
+    </div>
+  );
+};
+
+export default Footer;
