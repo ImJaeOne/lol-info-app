53c07d9a14c34aa5cad27b491cc1e9a5ae3bf416
ImJaeOne <dlawi0108@naver.com>
2025-03-11 23:42:27 +0900

    ♻️ refactor: championRotation api 로직 분리

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
+import { getChampionRotation } from "@/services/championService";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-
-const getChampionRotation = async () => {
-  const res = await fetch("/api/rotation");
-  if (!res.ok) {
-    throw new Error("챔피언 로테이션을 받아오는데 실패하였습니다.");
-  }
-  const data = await res.json();
-  return data;
-};
 
 const RotationPage = () => {
   const { data } = useQuery({
--- src/services/championService.ts
+++ src/services/championService.ts
@@ -33,3 +33,12 @@ export const getChampionDetail = async (id: string) => {
 
   return { data, version };
 };
+
+export const getChampionRotation = async () => {
+  const res = await fetch("/api/rotation");
+  if (!res.ok) {
+    throw new Error("챔피언 로테이션을 받아오는데 실패하였습니다.");
+  }
+  const data = await res.json();
+  return data;
+};
