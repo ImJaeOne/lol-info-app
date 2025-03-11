fe24356cc62a4b88f47d51ae17aa75fbdf8f87f1
ImJaeOne <dlawi0108@naver.com>
2025-03-11 11:47:55 +0900

    🎉 Add: Champion 관련 타입 지정

---
+++ src/types/Champion.ts
@@ -0,0 +1,61 @@
+export type Champion = {
+  type: string;
+  format: string;
+  version: string;
+  // Record는 객체 타입의 키와 값을 정의 할 때 사용하는 유틸리티 타입
+  data: Record<string, ChampionDetail>;
+};
+
+export type ChampionDetail = {
+  version: string;
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: ChampionInfo;
+  image: ChampionImage;
+  tags: string[];
+  partype: string;
+  stats: ChampionStats;
+};
+
+export type ChampionInfo = {
+  attack: number;
+  defense: number;
+  magic: number;
+  difficulty: number;
+};
+
+export type ChampionStats = {
+  hp: number;
+  hpperlevel: number;
+  mp: number;
+  mpperlevel: number;
+  movespeed: number;
+  armor: number;
+  armorperlevel: number;
+  spellblock: number;
+  spellblockperlevel: number;
+  attackrange: number;
+  hpregen: number;
+  hpregenperlevel: number;
+  mpregen: number;
+  mpregenperlevel: number;
+  crit: number;
+  critperlevel: number;
+  attackdamage: number;
+  attackdamageperlevel: number;
+  attackspeedperlevel: number;
+  attackspeed: number;
+};
+
+export type ChampionImage = {
+  full: string;
+  sprite: string;
+  group: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+};
