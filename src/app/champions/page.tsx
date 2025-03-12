e519baea758d3ff02eb5b224029c97cff862f5d2
ImJaeOne <dlawi0108@naver.com>
2025-03-12 21:26:35 +0900

    ✨ Update: Suspense 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,18 +1,14 @@
-import ChampionCard from "@/components/champion/ChampionCard";
-import { getChampionList } from "@/services/championService";
+import { Suspense } from "react";
+import Loading from "./loading";
+import ChampionList from "@/components/champion/ChampionList";
 
 const ChampionPage = async () => {
-  const championList = await getChampionList();
-  const championListArr = Object.entries(championList);
-
   return (
     <main className="w-[90%]">
-      <h1>챔피언 목록</h1>
-      <div className="flex flex-wrap gap-4 justify-between">
-        {championListArr?.map(([key, detail]) => (
-          <ChampionCard key={key} detail={detail} />
-        ))}
-      </div>
+      <h1 className="text-4xl text-red-700 mb-4">챔피언 목록</h1>
+      <Suspense fallback={<Loading />}>
+        <ChampionList />
+      </Suspense>
     </main>
   );
 };
---
+++ src/components/champion/ChampionList.tsx
@@ -0,0 +1,17 @@
+import { getChampionList } from "@/services/championService";
+import ChampionCard from "./ChampionCard";
+
+const ChampionList = async () => {
+  const championList = await getChampionList();
+  const championListArr = Object.entries(championList);
+
+  return (
+    <div className="flex flex-wrap gap-4 justify-between">
+      {championListArr?.map(([key, detail]) => (
+        <ChampionCard key={key} detail={detail} />
+      ))}
+    </div>
+  );
+};
+
+export default ChampionList;
