5c08607c5e4805180f8326c51515c4173954f967
ImJaeOne <dlawi0108@naver.com>
2025-03-12 15:01:39 +0900

    🎨 Style: championDetail css

--- src/components/champion/ChampionDetail.tsx
+++ src/components/champion/ChampionDetail.tsx
@@ -1,28 +1,43 @@
-import { PATH } from "@/constants/path"
-import { type ChampionDetail } from "@/types/Champion"
-import Image from "next/image"
+import { PATH } from "@/constants/path";
+import { type ChampionDetail } from "@/types/Champion";
+import Image from "next/image";
 
-const ChampionDetail = ({championDetail, version}: {championDetail: ChampionDetail, version: string}) => {
+const ChampionDetail = ({
+  championDetail,
+  version,
+}: {
+  championDetail: ChampionDetail;
+  version: string;
+}) => {
   return (
-    <div className="text-red-700 flex flex-col w-[60%]">
-          <span>{championDetail.name}</span>
-          <span>{championDetail.title}</span>
-          <Image
-            src={`${PATH.DDRAGON_URL}/cdn/${version}/img/champion/${championDetail.image.full}`}
-            width={80}
-            height={80}
-            alt={championDetail.name}
-            priority
-          />
-          <span>{championDetail.lore}</span>
-          <ul>
-            <li>공격력: {championDetail.info.attack}</li>
-            <li>방어력: {championDetail.info.defense}</li>
-            <li>마법력: {championDetail.info.magic}</li>
-            <li>난이도: {championDetail.info.difficulty}</li>
-          </ul>
-        </div>
-  )
-}
+    <div className=" flex flex-col w-[60%]">
+      <span className="text-4xl font-bold mb-4 text-red-700">
+        {championDetail.name}
+      </span>
+      <span className="text-2xl text-gray-600 mb-4">
+        {championDetail.title}
+      </span>
+      <div className="flex w-full justify-center mb-4">
+        <Image
+          src={`${PATH.DDRAGON_URL}/cdn/${version}/img/champion/${championDetail.image.full}`}
+          width={150}
+          height={150}
+          alt={championDetail.name}
+          priority
+        />
+      </div>
+      <span className="text-red-600 mb-4">{championDetail.lore}</span>
+      <div>
+        <span className="text-gray-600 text-xl">스탯</span>
+        <ul className="text-gray-500">
+          <li>공격력: {championDetail.info.attack}</li>
+          <li>방어력: {championDetail.info.defense}</li>
+          <li>마법력: {championDetail.info.magic}</li>
+          <li>난이도: {championDetail.info.difficulty}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default ChampionDetail
+export default ChampionDetail;
