eefe9ab7d9dd5ddbb8438c99eb9efcb9b1cc6fcc
ImJaeOne <dlawi0108@naver.com>
2025-03-12 17:35:12 +0900

    ✨ Update: base-url constants 지정

--- src/constants/path.js
+++ src/constants/path.js
@@ -2,4 +2,5 @@ export const PATH = {
   DDRAGON_URL: "https://ddragon.leagueoflegends.com",
   ROTATION_URL:
     "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+  BASE_URL: "http://localhost:3000",
 };
