6dfdcdf4dde9f49e0d12886e7bdfab78c2c5eb88
ImJaeOne <dlawi0108@naver.com>
2025-03-14 17:51:36 +0900

    try deploy

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -7,7 +7,6 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function makeQueryClient() {
   return new QueryClient({
