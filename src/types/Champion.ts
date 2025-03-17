afdaf799f69d450124dd0416a4b27dc85d65308a
ImJaeOne <dlawi0108@naver.com>
2025-03-17 16:28:11 +0900

    ♻️ refactor: 불필요한 type 삭제

--- src/types/Champion.ts
+++ src/types/Champion.ts
@@ -1,8 +1,5 @@
 export type Champion = {
-  type: string;
-  format: string;
   version: string;
-  // Record는 객체 타입의 키와 값을 정의 할 때 사용하는 유틸리티 타입
   data: Record<string, ChampionsDetail>;
 };
 
@@ -13,11 +10,7 @@ export type ChampionsDetail = {
   name: string;
   title: string;
   blurb: string;
-  info: ChampionInfo;
   image: ChampionImage;
-  tags: string[];
-  partype: string;
-  stats: ChampionStats;
 };
 
 export type ChampionDetail = {
@@ -27,49 +20,9 @@ export type ChampionDetail = {
   title: string;
   image: {
     full: string;
-    sprite: string;
-    group: string;
   };
   lore: string;
-  info: ChampionInfo;
   spells: ChampionSpells[];
-  passive: {
-    name: string;
-    description: string;
-    image: {
-      full: string;
-    };
-  };
-};
-
-export type ChampionInfo = {
-  attack: number;
-  defense: number;
-  magic: number;
-  difficulty: number;
-};
-
-export type ChampionStats = {
-  hp: number;
-  hpperlevel: number;
-  mp: number;
-  mpperlevel: number;
-  movespeed: number;
-  armor: number;
-  armorperlevel: number;
-  spellblock: number;
-  spellblockperlevel: number;
-  attackrange: number;
-  hpregen: number;
-  hpregenperlevel: number;
-  mpregen: number;
-  mpregenperlevel: number;
-  crit: number;
-  critperlevel: number;
-  attackdamage: number;
-  attackdamageperlevel: number;
-  attackspeedperlevel: number;
-  attackspeed: number;
 };
 
 export type ChampionSpells = {
--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,36 +1,14 @@
 export type Item = {
-    type: string;
-    version: string;
-    basic: object;
-    data: Record<string, ItemDetail>
-}
+  version: string;
+  data: Record<string, ItemDetail>;
+};
 
 export type ItemDetail = {
-    name: string;
-    description: string;
-    colloq: string;
-    plaintext: string;
-    into: string[];
-    image: ItemImage;
-    gold: ItemGold;
-    tags: string[];
-    maps: object;
-    stats: object;
-}
+  name: string;
+  plaintext: string;
+  image: ItemImage;
+};
 
 export type ItemImage = {
-    full: string;
-    sprite: string;
-    group: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-}
-
-export type ItemGold = {
-    base: number;
-    purchasable: boolean;
-    total: number;
-    sell: number;
-}
+  full: string;
+};
