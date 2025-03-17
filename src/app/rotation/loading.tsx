b3d29d546211b7396fc91f79683bebd491202c4f
ImJaeOne <dlawi0108@naver.com>
2025-03-17 16:03:51 +0900

    🎉 Add: rotation 로딩 추가

---
+++ src/app/rotation/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[100vh]">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-red-500 rounded-full animate-spin"></div>
+      <p className="mt-4 text-gray-600">로테이션을 불러오는 중...</p>
+    </div>
+  );
+};
+
+export default Loading;
